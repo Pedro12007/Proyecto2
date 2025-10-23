84c638dbf6400b9cc9e37e37c73066daf3aa8ed4
Pedro Monroy <monroy.pedroa07@gmail.com>
2025-10-22 18:50:19 -0600

    botones

--- admin.py
+++ admin.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 import sqlite3
 
-
 class Admin:
     def __init__(self):
         self.ventana_admin = tk.Tk()
@@ -9,3 +8,6 @@ class Admin:
         self.ventana_admin.state("zoomed")
         self.ventana_admin.geometry('900x800')
 
+        tk.Button(self.ventana_admin, text="Agregar usuario", font=("Arial", 16), bg="white", fg="black").pack(pady=20)
+        tk.Button(self.ventana_admin, text="Modificar datos de usuario", font=("Arial", 16), bg="white", fg="black").pack(pady=20)
+        tk.Button(self.ventana_admin, text="Cerrar sesión", font=("Arial", 16), bg="white", fg="black").pack(pady=20)
